06f7a1af0ad10d9457b9a02fd0331e1d0e1899fa
Paul Egboh <egbohpaul@gmail.com>
2026-05-26 01:20:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+import os
 import logging
 
 app = Flask(__name__)
-
 logging.basicConfig(level=logging.INFO)
 
 @app.route("/")
 def home():
-    app.logger.info("Home endpoint was called")
-    return "✅ Hello from my Flask app running on Azure!"
+    app_name = os.getenv("APP_NAME", "Default App Name")
+    app.logger.info("Home endpoint called, APP_NAME=%s", app_name)
+    return f"✅ Hello from {app_name}!"
 
 @app.route("/health")
 def health():
-    app.logger.info("Health check called from %s", request.remote_addr)
-    return jsonify(status="OK", service="flask-app")
+    return jsonify(status="OK")
 
 if __name__ == "__main__":
     app.run()
