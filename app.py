7e8597ae50172d6f1ecc893e054fd9ab282e4b1c
Paul Egboh <egbohpaul@gmail.com>
2026-05-27 00:47:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask, jsonify
 import os
 import logging
+from azure.identity import ManagedIdentityCredential
+from azure.keyvault.secrets import SecretClient
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
+KEY_VAULT_NAME = os.getenv("KEY_VAULT_NAME")
+KV_URI = f"https://{KEY_VAULT_NAME}.vault.azure.net"
+
+credential = ManagedIdentityCredential()
+client = SecretClient(vault_url=KV_URI, credential=credential)
+
 @app.route("/")
 def home():
-    app_name = os.getenv("APP_NAME", "Default App Name")
-    environment = os.getenv("ENVIRONMENT", "dev")
-
-    app.logger.info(
-        "Home endpoint called | APP_NAME=%s | ENVIRONMENT=%s",
-        app_name,
-        environment
-    )
-
-    if environment == "prod":
-        return f"✅ Welcome to {app_name}"
-    else:
-        return f"🧪 [{environment.upper()}] {app_name}"
+    secret = client.get_secret("api-message").value
+    app.logger.info("Read secret from Key Vault")
+    return f"✅ {secret}"
 
 @app.route("/health")
 def health():
